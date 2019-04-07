9964e66274207a5bb540e58d9ae67b73b5da6ead
Jesstern Rays <jsstrn@users.noreply.github.com>
2019-04-07 22:41:29 +0800

    Save file to database

--- routes/upload.js
+++ routes/upload.js
@@ -4,13 +4,41 @@ const router = express.Router();
 const multer = require("multer");
 
 const upload = multer({
-  dest: "./public/uploads/"
+  storage: multer.memoryStorage()
 });
 
+const mongoose = require("mongoose");
+const dataSchema = new mongoose.Schema({
+  binData: {
+    type: Buffer,
+    required: true
+  }
+});
+
+const Data = mongoose.model("Data", dataSchema);
+
 router.route("/").post(upload.single("file"), (req, res) => {
-  console.log("🗄 File metadata", req.file);
-  const {destination, originalname, mimetype} = req.file;
-  return res.send(`👍 Successfully uploaded ${originalname} to ${destination}`);
+  console.log("🗄 File metadata", req.file, req.file.buffer);
+
+  const {
+    buffer,
+    filename,
+    destination,
+    originalname,
+    mimetype,
+    size
+  } = req.file;
+  const data = new Data({ binData: buffer });
+
+  data.save((err, file) => {
+    if (err) {
+      return res.send(500, err);
+    }
+    return res.send(
+      201,
+      `👍 Successfully uploaded ${originalname} to ${destination}`
+    );
+  });
 });
 
 module.exports = router;
