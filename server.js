b92ac69571ff965bc85d11d018a5d2312185ec73
nipunbatra1804 <nipunbatra1994@gmail.com>
2019-04-10 15:08:01 +0800

    remodel seeding from app.listen

--- server.js
+++ server.js
@@ -1,18 +1,6 @@
 /* eslint-disable no-console */
-
 const app = require('./app');
 const mongoose = require('mongoose');
-const Category = require('./models/Category');
-const Action = require('./models/Action');
-const Level = require('./models/Level');
-const Introspection = require('./models/Introspection');
-const {
-  actionSeed,
-  levelSeed,
-  categorySeed,
-  seedIntrospection
-} = require('./tests/fixtures/seed');
-
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config();
 }
@@ -40,14 +28,6 @@ db.once('connected', () => {
     if (process.env.NODE_ENV === 'production') {
       console.log(`Server is running on Heroku with port number ${port}`);
     } else {
-      await Introspection.deleteMany({});
-      await Action.deleteMany({});
-      await Category.deleteMany({});
-      await Level.deleteMany({});
-      await Action.insertMany(actionSeed);
-      await Category.insertMany(categorySeed);
-      await Level.insertMany(levelSeed);
-      await seedIntrospection();
       console.log(`Server is running on http://localhost:${port}`);
     }
   });
